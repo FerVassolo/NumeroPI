15287ac1d38dbd2b8f7b26466f5684b35c39ebad
Parrandos <63514959+Parrandos@users.noreply.github.com>
2022-08-15 17:24:18 -0300

    Initial commit

---
+++ lib/Gauss-Legendre.py
@@ -0,0 +1,23 @@
+import math
+
+def solve(a, b, t, p, n): # newA, newB quiere decir An+1, Bn+1. La variable x ya es Xn+1
+    if (n > 0):
+        x = (a+b)/2
+        newA = x
+        newB = math.sqrt(a*b)
+        t = t - p*math.pow(a-x, 2)
+        pi = math.pow(newA+newB, 2)/(4*t)
+        p = 2*p
+        print(pi)
+        n -= 1
+        return solve(newA, newB, t, p, n)
+solve(1, 1/math.sqrt(2), 1/4, 1, 4)
+
+
+
+
+
+
+
+
+
