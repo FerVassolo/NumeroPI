5a18cfa1c696dd1cc567e79e0df2b5de1b1d0767
FerVassolo <91639077+FerVassolo@users.noreply.github.com>
2022-08-15 18:37:41 -0300

    Update Gauss-Legendre.py

--- Gauss-Legendre.py
+++ Gauss-Legendre.py
@@ -1,4 +1,5 @@
 import math
+import decimal
 
 def solve(a, b, t, p, n): # newA, newB quiere decir An+1, Bn+1. La variable x ya es Xn+1
     if (n > 0):
@@ -12,3 +13,26 @@ def solve(a, b, t, p, n): # newA, newB quiere decir An+1, Bn+1. La variable x ya
         n -= 1
         return solve(newA, newB, t, p, n)
 solve(1, 1/math.sqrt(2), 1/4, 1, 4)
+
+
+def conDecimal(a, b, t, p, n):
+    D = decimal.Decimal
+    a = a
+    b = b
+    t = t
+    p = p
+    if (n > 0):
+        x = (a+b)/2
+        newA = x
+        newB = (a*b).sqrt()
+        t = t - p*(a-x)*(a-x)
+        pi = ((newA+newB)*(newA+newB))/(4*t)
+        p = 2*p
+        print(pi)
+        n -= 1
+        return conDecimal(newA, newB, t, p, n)
+
+D = decimal.Decimal
+conDecimal(1, 1/D(2).sqrt(), 1/D(4), 1, 5)
+print(3,141592653589793238462643383279502884197169399375105)
+
