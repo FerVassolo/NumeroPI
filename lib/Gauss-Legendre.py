50a8062b5f5fad7a99a3999cc761fab3af39ca29
Parrandos <63514959+Parrandos@users.noreply.github.com>
2022-08-15 19:11:06 -0300

    .

--- lib/Gauss-Legendre.py
+++ lib/Gauss-Legendre.py
@@ -8,10 +8,14 @@ def solve(a, b, t, p, n): # newA, newB quiere decir An+1, Bn+1. La variable x ya
         t = t - p*math.pow(a-x, 2)
         pi = math.pow(newA+newB, 2)/(4*t)
         p = 2*p
-        print(pi)
+
         n -= 1
-        return solve(newA, newB, t, p, n)
-solve(1, 1/math.sqrt(2), 1/4, 1, 4)
+        if n != 0:
+            return solve(newA, newB, t, p, n)
+        else:
+            return pi
+
+print(solve(1, 1/math.sqrt(2), 1/4, 1, 4))
 print(3,141592653589793238462643383279502884197169399375105)
 
 
@@ -28,16 +32,12 @@ def conDecimal(a, b, t, p, n):
         t = t - p*(a-x)*(a-x)
         pi = ((newA+newB)*(newA+newB))/(4*t)
         p = 2*p
-        print(pi)
+
         n -= 1
-        return conDecimal(newA, newB, t, p, n)
+        if n != 0:
+            return conDecimal(newA, newB, t, p, n)
+        else:
+            return pi
 
 D = decimal.Decimal
-conDecimal(1, 1/D(2).sqrt(), 1/D(4), 1, 5)
-print(3,141592653589793238462643383279502884197169399375105)
-
-
-
-
-
-
+print(conDecimal(1, 1/D(2).sqrt(), 1/D(4), 1, 5))
