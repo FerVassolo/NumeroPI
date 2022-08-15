92ecaa3440d051e9db1bd8183e7c9f348011afcd
Parrandos <63514959+Parrandos@users.noreply.github.com>
2022-08-15 19:40:31 -0300

    Agregue al main el analisis con decimals

--- Main.py
+++ Main.py
@@ -1,14 +1,17 @@
 from PiMethods import mathLibraryPi, spigotAlgorithm, GaussLegendre
 from Circle.circle import Circle
 import math
+import decimal
 
 
-gausslegendreValue = GaussLegendre.solve(1, 1/math.sqrt(2), 1/4, 1, 3)
-spigotValue = spigotAlgorithm.SpigotAlgorithm.calculate(6)
+gausslegendreValue = GaussLegendre.solve(1, 1/math.sqrt(2), 1/4, 1, 4)
+D = decimal.Decimal
+gausslegendreDecimals = GaussLegendre.conDecimal(1, 1/D(2).sqrt(), 1/D(4), 1, 5)
+spigotValue = spigotAlgorithm.SpigotAlgorithm.calculate(40)
 
 def comparisson():
     print("Distintos valores de pi:")
-    print(f"\tMath Library: \n\t\t{mathLibraryPi.getPi()}\n\tSpigot: \n\t\t{spigotValue}\n\tGauss-Legendre: \n\t\t{gausslegendreValue}")
+    print(f"\tMath Library: \n\t\t{mathLibraryPi.getPi()}\n\tSpigot: \n\t\t{spigotValue}\n\tGauss-Legendre: \n\t\t{gausslegendreValue}\n\tGauss-Legendre con decimals: \n\t\t{gausslegendreDecimals}")
 
 comparisson()
 
@@ -18,4 +21,4 @@ print("Areas para los distintos valores de pi:")
 print(f"\tMath Library Area: {circle1.calculateArea(mathLibraryPi.getPi())}")
 print(f"\tSpigot Area: {circle1.calculateArea(spigotValue)}")
 print(f"\tGauss-Legendre Area: {circle1.calculateArea(gausslegendreValue)}")
-
+print(f"\tGauss-Legendre con decimals Area: {circle1.calculateArea(gausslegendreDecimals)}")
